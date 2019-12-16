f7380f31637b33a6caabbd361cb8ae664a7ed70b
kp <kamal.patel30@hotmail.com>
2019-12-15 19:06:21 -0500

    reverse words

---
+++ reverse.py
@@ -0,0 +1,20 @@
+def reverse_string(string):
+    if len(string) <= 1:
+        return string
+    return string[-1] + reverse_string(string[-1])
+
+def reverse_word(string):
+    curr_word = ' '
+    results = ''
+
+    for char in string:
+        if char == ' ' or char == '\t':
+            if not curr_word == '':
+                results += reverse_string(curr_word)
+                curr_word = ''
+            results += char
+        else:
+            curr_word += char
+
+    results += reverse_string(curr_word)
+    return results
